18824ed8ebe34cb272e6f1afae7f5501d7a0b2b5
GamerChris0837 <jjangga4227@gmail.com>
2023-09-16 11:41:29 +0900

    Update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ screen = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption('2048')
 timer = pygame.time.Clock()
 fps = 30
-font = pygame.font.Font('freesansbold.ttf', 24)
+font = pygame.font.Font('KoPub Dotum Bold.ttf', 24)
 
 colors = {0: (204, 192, 179),
           2: (238, 228, 218),
@@ -41,10 +41,10 @@ high_score = init_high
 
 def draw_over():
     pygame.draw.rect(screen, 'black', [50, 50, 300, 100], 0, 10)
-    game_over_text1 = font.render('Game Over!', True, 'white')
-    game_over_text2 = font.render('Press Enter to Restart', True, 'white')
-    screen.blit(game_over_text1, (130, 65))
-    screen.blit(game_over_text2, (70, 105))
+    game_over_text1 = font.render('게임 오버!', True, 'white')
+    game_over_text2 = font.render('Enter키를 눌러 재시작하세요', True, 'white')
+    screen.blit(game_over_text1, (155, 65))
+    screen.blit(game_over_text2, (60, 105))
 
 def take_turn(direc, board):
     global score
@@ -52,73 +52,73 @@ def take_turn(direc, board):
     if direc == 'UP':
         for i in range(4):
             for j in range(4):
-                shift = 0
+                s = 0
                 if i > 0:
                     for q in range(i):
                         if board[q][j] == 0:
-                            shift += 1
-                    if shift > 0:
-                        board[i - shift][j] = board[i][j]
+                            s += 1
+                    if s > 0:
+                        board[i - s][j] = board[i][j]
                         board[i][j] = 0
-                    if board[i - shift - 1][j] == board[i - shift][j] and not merged[i - shift][j] \
-                            and not merged[i - shift - 1][j]:
-                        board[i - shift - 1][j] *= 2
-                        score += board[i - shift - 1][j]
-                        board[i - shift][j] = 0
-                        merged[i - shift - 1][j] = True
+                    if board[i - s - 1][j] == board[i - s][j] and not merged[i - s][j] \
+                            and not merged[i - s - 1][j]:
+                        board[i - s - 1][j] *= 2
+                        score += board[i - s - 1][j]
+                        board[i - s][j] = 0
+                        merged[i - s - 1][j] = True
 
     elif direc == 'DOWN':
         for i in range(3):
             for j in range(4):
-                shift = 0
+                s = 0
                 for q in range(i + 1):
                     if board[3 - q][j] == 0:
-                        shift += 1
-                if shift > 0:
-                    board[2 - i + shift][j] = board[2 - i][j]
+                        s += 1
+                if s > 0:
+                    board[2 - i + s][j] = board[2 - i][j]
                     board[2 - i][j] = 0
-                if 3 - i + shift <= 3:
-                    if board[2 - i + shift][j] == board[3 - i + shift][j] and not merged[3 - i + shift][j] \
-                            and not merged[2 - i + shift][j]:
-                        board[3 - i + shift][j] *= 2
-                        score += board[3 - i + shift][j]
-                        board[2 - i + shift][j] = 0
-                        merged[3 - i + shift][j] = True
+                if 3 - i + s <= 3:
+                    if board[2 - i + s][j] == board[3 - i + s][j] and not merged[3 - i + s][j] \
+                            and not merged[2 - i + s][j]:
+                        board[3 - i + s][j] *= 2
+                        score += board[3 - i + s][j]
+                        board[2 - i + s][j] = 0
+                        merged[3 - i + s][j] = True
 
     elif direc == 'LEFT':
         for i in range(4):
             for j in range(4):
-                shift = 0
+                s = 0
                 for q in range(j):
                     if board[i][q] == 0:
-                        shift += 1
-                if shift > 0:
-                    board[i][j - shift] = board[i][j]
+                        s += 1
+                if s > 0:
+                    board[i][j - s] = board[i][j]
                     board[i][j] = 0
-                if board[i][j - shift] == board[i][j - shift - 1] and not merged[i][j - shift - 1] \
-                        and not merged[i][j - shift]:
-                    board[i][j - shift - 1] *= 2
-                    score += board[i][j - shift - 1]
-                    board[i][j - shift] = 0
-                    merged[i][j - shift - 1] = True
+                if board[i][j - s] == board[i][j - s - 1] and not merged[i][j - s - 1] \
+                        and not merged[i][j - s]:
+                    board[i][j - s - 1] *= 2
+                    score += board[i][j - s - 1]
+                    board[i][j - s] = 0
+                    merged[i][j - s - 1] = True
 
     elif direc == 'RIGHT':
         for i in range(4):
             for j in range(4):
-                shift = 0
+                s = 0
                 for q in range(j):
                     if board[i][3 - q] == 0:
-                        shift += 1
-                if shift > 0:
-                    board[i][3 - j + shift] = board[i][3 - j]
+                        s += 1
+                if s > 0:
+                    board[i][3 - j + s] = board[i][3 - j]
                     board[i][3 - j] = 0
-                if 4 - j + shift <= 3:
-                    if board[i][4 - j + shift] == board[i][3 - j + shift] and not merged[i][4 - j + shift] \
-                            and not merged[i][3 - j + shift]:
-                        board[i][4 - j + shift] *= 2
-                        score += board[i][4 - j + shift]
-                        board[i][3 - j + shift] = 0
-                        merged[i][4 - j + shift] = True
+                if 4 - j + s <= 3:
+                    if board[i][4 - j + s] == board[i][3 - j + s] and not merged[i][4 - j + s] \
+                            and not merged[i][3 - j + s]:
+                        board[i][4 - j + s] *= 2
+                        score += board[i][4 - j + s]
+                        board[i][3 - j + s] = 0
+                        merged[i][4 - j + s] = True
     return board
 
 def new_pieces(board):
@@ -139,8 +139,8 @@ def new_pieces(board):
 
 def draw_board():
     pygame.draw.rect(screen, colors['bg'], [0, 0, 400, 400], 0, 10)
-    score_text = font.render(f'Score: {score}', True, 'black')
-    high_score_text = font.render(f'High Score: {high_score}', True, 'black')
+    score_text = font.render(f'점수: {score}', True, 'black')
+    high_score_text = font.render(f'최고 점수: {high_score}', True, 'black')
     screen.blit(score_text, (10, 410))
     screen.blit(high_score_text, (10, 450))
     pass
@@ -160,7 +160,7 @@ def draw_pieces(board):
             pygame.draw.rect(screen, color, [j * 95 + 20, i * 95 + 20, 75, 75], 0, 5)
             if value > 0:
                 value_len = len(str(value))
-                font = pygame.font.Font('freesansbold.ttf', 48 - (5 * value_len))
+                font = pygame.font.Font('KoPub Dotum Bold.ttf', 48 - (5 * value_len))
                 value_text = font.render(str(value), True, value_color)
                 text_rect = value_text.get_rect(center=(j * 95 + 57, i * 95 + 57))
                 screen.blit(value_text, text_rect)
@@ -210,7 +210,7 @@ while run:
                     direction = ''
                     game_over = False
 
-    if score > high_score:
+    if score >= high_score:
         high_score = score
 
     pygame.display.flip()
